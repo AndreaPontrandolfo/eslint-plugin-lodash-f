4a8d4c065e8123a5cbaa3a5e6524005ef8036ddf
AndreaPontrandolfo <andrea.pontra@gmail.com>
2022-02-27 00:32:22 +0100

    implemented more tests for prefer-is-empty

--- src/rules/prefer-is-empty.js
+++ src/rules/prefer-is-empty.js
@@ -15,74 +15,22 @@ module.exports = {
         schema: [],
         docs: {
             url: getDocsUrl('prefer-is-empty')
-        }
+        },
+        fixable: 'code'
     },
 
     create(context) {
-        // const {isNegationExpression, isEquivalentMemberExp} = require('../util/astUtil')
-        const {/*isCallToLodashMethod,*/ getLodashContext} = require('../util/lodashUtil')
-        // const _ = require('lodash')
+        const {getLodashContext} = require('../util/lodashUtil')
         const lodashContext = getLodashContext(context)
-        // const nilChecks = {
-        //     null: {
-        //         isValue: _.matches({type: 'Literal', value: null}),
-        //         expressionChecks: [getLodashTypeCheckedBy('isNull'), getValueComparedTo('null')]
-        //     },
-        //     undefined: {
-        //         isValue: _.matches({type: 'Identifier', name: 'undefined'}),
-        //         expressionChecks: [getLodashTypeCheckedBy('isUndefined'), getValueComparedTo('undefined'), getValueWithTypeofUndefinedComparison]
-        //     }
-        // }
 
-        // function getLodashTypeCheckedBy(typecheck) {
-        //     return function (node) {
-        //         return isCallToLodashMethod(node, typecheck, lodashContext) && node.arguments[0]
-        //     }
-        // }
-
-        // function getValueComparedTo(nil) {
-        //     return function (node, operator) {
-        //         return node.type === 'BinaryExpression' && node.operator === operator &&
-        //             ((nilChecks[nil].isValue(node.right) && node.left) || (nilChecks[nil].isValue(node.left) && node.right))
-        //     }
-        // }
-
-
-        // const getTypeofArgument = _.cond([
-        //     [_.matches({type: 'UnaryExpression', operator: 'typeof'}), _.property('argument')]
-        // ])
-
-        // const isUndefinedString = _.matches({type: 'Literal', value: 'undefined'})
-        
-        // const isZero = _.matches({type: 'Literal', value: 0})
-
-        // function getValueWithTypeofUndefinedComparison(node, operator) {
-        //     return node.type === 'BinaryExpression' && node.operator === operator &&
-        //         ((isUndefinedString(node.right) && getTypeofArgument(node.left)) ||
-        //         (isUndefinedString(node.left) && getTypeofArgument(node.right)))
-        // }
-
-        // function checkExpression(nil, operator, node) {
-        //     return _(nilChecks[nil].expressionChecks)
-        //         .map(check => check(node, operator))
-        //         .find()
-        // }
-
-        // function checkNegatedExpression(nil, node) {
-        //     return (isNegationExpression(node) && checkExpression(nil, '===', node.argument)) || checkExpression(nil, '!==', node)
-        // }
-
-        // function isEquivalentExistingExpression(node, leftNil, rightNil) {
-        //     const leftExp = checkExpression(leftNil, '===', node.left)
-        //     return leftExp && isEquivalentMemberExp(leftExp, checkExpression(rightNil, '===', node.right))
-        // }
-
-        // const getLeftMember = () => node.type === 'MemberExpression'
-
-        // function isEquivalentExistingNegation(node, leftNil, rightNil) {
-        //     const leftExp = checkNegatedExpression(leftNil, node.left)
-        //     return leftExp && isEquivalentMemberExp(leftExp, checkNegatedExpression(rightNil, node.right))
-        // }
+        function getTextOfNode(node) {
+            if (node) {
+                if (node.type === 'Identifier') {
+                    return node.name
+                }
+                return context.getSourceCode().getText(node)
+            }
+        }        
 
         const visitors = lodashContext.getImportVisitors()
         visitors.BinaryExpression = function (node) {
@@ -90,27 +38,22 @@ module.exports = {
                 const leftExpressionMember = node.left.property.name 
                 const rightExpressionMember = node.right.value
                 if (leftExpressionMember === 'length' && rightExpressionMember === 0) {
-                    context.report({node, message: 'Prefer isEmpty over manually checking for length value.'})
+                    const subjectObject = node.left.object
+                    context.report({node, message: 'Prefer isEmpty over manually checking for length value.', fix(fixer) {
+                        return fixer.replaceText(node, `isEmpty(${getTextOfNode(subjectObject)})`)
+                    }})
                 }
             } 
             if (node.operator === '>') {
                 const leftExpressionMember = node.left.property.name 
                 const rightExpressionMember = node.right.value
                 if (leftExpressionMember === 'length' && rightExpressionMember === 0) {
-                    context.report({node, message: 'Prefer isEmpty over manually checking for length value.'})
-                }
-            } 
-
-            // if (node.operator === '===') {
-            //     if (isEquivalentExistingExpression(node, 'undefined', 'null') ||
-            //         isEquivalentExistingExpression(node, 'null', 'undefined')) {
-            //         context.report({node, message: 'Prefer isEmpty over manually checking for length value.'})
-            //     }
-            // } 
-            // else if (isEquivalentExistingNegation(node, 'undefined', 'null') ||
-            //     isEquivalentExistingNegation(node, 'null', 'undefined')) {
-            //     context.report({node, message: 'Prefer isNil over checking for undefined or null.'})
-            // }
+                    const subjectObject = node.left.object
+                    context.report({node, message: 'Prefer isEmpty over manually checking for length value.', fix(fixer) {
+                        return fixer.replaceText(node, `!isEmpty(${getTextOfNode(subjectObject)})`)
+                    }})
+                } 
+            }            
         }
         return visitors
     }
--- tests/lib/rules/prefer-is-empty.js
+++ tests/lib/rules/prefer-is-empty.js
@@ -23,9 +23,19 @@ ruleTester.run('prefer-is-empty', rule, {
         'const myLengthEqualZero = myVar;'
     ].map(withDefaultPragma),
     invalid: [
-        'const myLengthEqualZero = myVar.length === 0;',
-        'const myLengthEqualZero = myVar.length > 0;',
-        'const myLengthEqualZero = myVar.myProp.mySecondProp.length === 0;',
-        'const myLengthEqualZero = myVar.myProp.mySecondProp.length > 0;'
+        {code: 'const myLengthEqualZero = myVar.length === 0;', output: 'const myLengthEqualZero = isEmpty(myVar);'},
+        {code: 'const myLengthEqualZero = myVar.length > 0;', output: 'const myLengthEqualZero = !isEmpty(myVar);'},
+        {code: 'const myLengthEqualZero = myVar.length > 0 ? "first" : "second";', output: 'const myLengthEqualZero = !isEmpty(myVar) ? "first" : "second";'},
+        {code: 'const myLengthEqualZero = myVar.myProp.mySecondProp.length === 0;', output: 'const myLengthEqualZero = isEmpty(myVar.myProp.mySecondProp);'},
+        {code: 'const myLengthEqualZero = myVar.myProp.mySecondProp.length > 0;', output: 'const myLengthEqualZero = !isEmpty(myVar.myProp.mySecondProp);'},
+        {code: `
+        const xprop = "x"
+        const yprop = "y"
+        const myLengthWithOCWithProp = myvar[xprop].mySecondprop.myThirdProp[yprop].length > 0;
+        `, output: `
+        const xprop = "x"
+        const yprop = "y"
+        const myLengthWithOCWithProp = !isEmpty(myvar[xprop].mySecondprop.myThirdProp[yprop]);
+        `}
     ].map(withDefaultPragma).map(toErrorObject)
 })
